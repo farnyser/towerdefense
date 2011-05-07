9ba1dab76911fb3be7bff9a4a75774892670c6d3
Serge Farny <contact@scawf.info>
2011-05-07 09:17:27 +0200

    Separation Object/Enemy/Tower.

---
+++ enemy.hpp
@@ -0,0 +1,34 @@
+#ifndef LO21_ENEMY
+#define LO21_ENEMY
+
+#include "object.hpp"
+
+class Enemy: public Object
+{
+private:
+    float hp;
+    int size;
+
+};
+
+class Ant: public Enemy
+{
+
+};
+
+class Bug: public Enemy
+{
+
+};
+
+class Bee: public Enemy
+{
+
+};
+
+class Mosquito: public Enemy
+{
+
+};
+
+#endif /* end of include guard: LO21_ENEMY */
---
+++ tower.hpp
@@ -0,0 +1,37 @@
+#ifndef LO21_TOWER
+#define LO21_TOWER
+
+#include "object.hpp"
+
+class Tower: public Object
+{
+private:
+	int level;
+};
+
+class WaterGun: public Tower
+{
+
+};
+
+class Slingshot: public Tower
+{
+
+};
+
+class PaintBall: public Tower
+{
+
+};
+
+class PetanquePlayer: public Tower
+{
+
+};
+
+class Musician: public Tower
+{
+
+};
+
+#endif /* end of include guard: LO21_TOWER */
