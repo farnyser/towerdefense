dc6ff5d8b7cfc73964c09f3bc6c06ba27b78d9ab
Paul Grandperrin <paul.grandperrin@gmail.com>
2011-05-19 12:03:24 +0200

    Ajout de l'ui du dock crée avec QtDesigner

--- src/dock.cpp
+++ src/dock.cpp
@@ -1,6 +1,7 @@
 #include "dock.hpp"
 
-Dock::Dock(QWidget* parent, Qt::WindowFlags flags): QDockWidget(tr("Options"),parent, flags)
+Dock::Dock(QWidget* parent, Qt::WindowFlags flags): QDockWidget(tr("Options"),parent, flags),ui(new Ui_DockWidget)
 {
+	ui->setupUi(this);
 	
 }
--- src/dock.hpp
+++ src/dock.hpp
@@ -2,12 +2,15 @@
 #define DOCK_HPP
 
 #include <qdockwidget.h>
-
+#include "ui_dock.h"
 
 class Dock : public QDockWidget
 {
 public:
 	explicit Dock(QWidget* parent = 0, Qt::WindowFlags flags = 0);
+	
+private:
+	Ui_DockWidget *ui;
 };
 
 #endif // DOCK_HPP
